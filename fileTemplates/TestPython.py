8f9dfdf22cf376c7d97a20cadccba32dc8acebd9
lokesh.govindu <lokesh.govindu@RIL1506DRLB152.rolta.com>
2019-08-14 14:38:40 +0530

    IC-2019.2 <lokesh.govindu@RIL1506DRLB152 Create baseRefactoring.xml, ide.general.xml, project.default.xml, rainbow_brackets.xml, TestPython.py, Default.xml, applicationLibraries.xml, file.template.settings.xml, find.xml, ui.lnf.xml, textmate_os.xml, filetypes.xml, projectView.xml, LoGo_NoSpaces.xml, LoGo.icls, keymap.xml, code.style.schemes.xml, Deployment.xml, LoGo.xml, colors.scheme.xml, KeyPromoterXSettings.xml, notifications.xml, scala_config.xml, customization.xml, GrepConsole.xml, abbreviations.xml, output.xml, Default.xml, scala.xml, laf.xml, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, LoGo_Spaces.xml, customPostfixTemplates.xml, dbnavigator.xml, codota.xml, editor.codeinsight.xml, iterations.xml, editor.xml, usageView.xml, debugger.xml, studioSettings.xml, sonarlint.xml, updates.xml

---
+++ fileTemplates/TestPython.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    print('=== BEGIN ===')
+
+    exitCode = main()
+
+    print("=== END ===")
+    sys.exit(exitCode)
